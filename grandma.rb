bc629fbb4937d0af15feaf2e2ab4bbf2628cee59
Henry Allan <henryallan50@gmail.com>
2019-10-21 18:15:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,10 +17,11 @@ def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
   
-  
+    p "I LOVE YOU TOO PUMPKIN!"
+    
   elsif phrase == phrase.upcase
     
-    
+    p "NO, NOT SINCE 1938!"
   
   else
     
